423b145f461c50b1b17679c3e62e12f02f4b01be
Azat S <to@azat.io>
2023-05-28 00:02:45 +0300

    fix: update url to documentation of rules

--- utils/create-eslint-rule.ts
+++ utils/create-eslint-rule.ts
@@ -1,8 +1,5 @@
 import { ESLintUtils } from '@typescript-eslint/utils'
 
-import { repository } from '~/package.json'
-
 export let createEslintRule = ESLintUtils.RuleCreator(
-  ruleName =>
-    `https://github.com/${repository}/blob/main/docs/rules/${ruleName}.md`,
+  ruleName => `https://eslint-plugin-perfectionist.azat.io/rules/${ruleName}`,
 )
