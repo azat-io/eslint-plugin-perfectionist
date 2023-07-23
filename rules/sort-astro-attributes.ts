7a792cc9f6c791378f313edcbfb7c179a8d5fe7e
Azat S <to@azat.io>
2023-07-23 17:04:50 +0300

    refactor: add use-groups helper

--- rules/sort-astro-attributes.ts
+++ rules/sort-astro-attributes.ts
@@ -1,14 +1,15 @@
 import type { TSESTree } from '@typescript-eslint/types'
 import type { AST } from 'astro-eslint-parser'
 
-import { minimatch } from 'minimatch'
 import path from 'path'
 
 import type { SortingNode } from '../typings'
 
 import { createEslintRule } from '../utils/create-eslint-rule'
+import { getGroupNumber } from '../utils/get-group-number'
 import { rangeToDiff } from '../utils/range-to-diff'
 import { SortOrder, SortType } from '../typings'
+import { useGroups } from '../utils/use-groups'
 import { makeFixes } from '../utils/make-fixes'
 import { sortNodes } from '../utils/sort-nodes'
 import { pairwise } from '../utils/pairwise'
@@ -21,10 +22,6 @@ type Group<T extends string[]> =
   | 'shorthand'
   | 'unknown'
   | T[number]
-
-type SortingNodeWithGroup<T extends string[]> = SortingNode & {
-  group: Group<T>
-}
 
 type MESSAGE_ID = 'unexpectedAstroAttributesOrder'
 
@@ -112,8 +109,8 @@ export default createEslintRule<Options<string[]>, MESSAGE_ID>({
 
           let source = context.getSourceCode()
 
-          let parts: SortingNodeWithGroup<string[]>[][] = attributes.reduce(
-            (accumulator: SortingNodeWithGroup<string[]>[][], attribute) => {
+          let parts: SortingNode[][] = attributes.reduce(
+            (accumulator: SortingNode[][], attribute) => {
               if (attribute.type === 'JSXSpreadAttribute') {
                 accumulator.push([])
                 return accumulator
@@ -124,28 +121,11 @@ export default createEslintRule<Options<string[]>, MESSAGE_ID>({
                   ? attribute.name.name
                   : source.text.slice(...attribute.name.range)
 
-              let group: Group<string[]> | undefined
+              let { getGroup, defineGroup, setCustomGroups } = useGroups(
+                options.groups,
+              )
 
-              let defineGroup = (nodeGroup: Group<string[]>) => {
-                if (!group && options.groups.flat().includes(nodeGroup)) {
-                  group = nodeGroup
-                }
-              }
-
-              for (let [key, pattern] of Object.entries(
-                options['custom-groups'],
-              )) {
-                if (
-                  Array.isArray(pattern) &&
-                  pattern.some(patternValue => minimatch(name, patternValue))
-                ) {
-                  defineGroup(key)
-                }
-
-                if (typeof pattern === 'string' && minimatch(name, pattern)) {
-                  defineGroup(key)
-                }
-              }
+              setCustomGroups(options['custom-groups'], name)
 
               if (attribute.type === 'AstroShorthandAttribute') {
                 defineGroup('astro-shorthand')
@@ -163,7 +143,7 @@ export default createEslintRule<Options<string[]>, MESSAGE_ID>({
               accumulator.at(-1)!.push({
                 size: rangeToDiff(attribute.range),
                 node: attribute as unknown as TSESTree.Node,
-                group: group ?? 'unknown',
+                group: getGroup(),
                 name,
               })
 
@@ -172,27 +152,10 @@ export default createEslintRule<Options<string[]>, MESSAGE_ID>({
             [[]],
           )
 
-          let getGroupNumber = (
-            nodeWithGroup: SortingNodeWithGroup<string[]>,
-          ): number => {
-            for (let i = 0, max = options.groups.length; i < max; i++) {
-              let currentGroup = options.groups[i]
-
-              if (
-                nodeWithGroup.group === currentGroup ||
-                (Array.isArray(currentGroup) &&
-                  currentGroup.includes(nodeWithGroup.group))
-              ) {
-                return i
-              }
-            }
-            return options.groups.length
-          }
-
           for (let nodes of parts) {
             pairwise(nodes, (left, right) => {
-              let leftNum = getGroupNumber(left)
-              let rightNum = getGroupNumber(right)
+              let leftNum = getGroupNumber(options.groups, left)
+              let rightNum = getGroupNumber(options.groups, right)
 
               if (
                 leftNum > rightNum ||
@@ -207,11 +170,11 @@ export default createEslintRule<Options<string[]>, MESSAGE_ID>({
                   node: right.node,
                   fix: fixer => {
                     let grouped: {
-                      [key: string]: SortingNodeWithGroup<string[]>[]
+                      [key: string]: SortingNode[]
                     } = {}
 
                     for (let currentNode of nodes) {
-                      let groupNum = getGroupNumber(currentNode)
+                      let groupNum = getGroupNumber(options.groups, currentNode)
 
                       if (!(groupNum in grouped)) {
                         grouped[groupNum] = [currentNode]
