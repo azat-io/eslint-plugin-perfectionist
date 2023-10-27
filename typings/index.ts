25c54b791cbd8a300914d28a640a67c4ac98e403
Taylore <tthornton3@chewy.com>
2023-10-26 22:45:56 -0400

    feat(sort-imports): max line length

--- typings/index.ts
+++ typings/index.ts
@@ -14,6 +14,7 @@ export enum SortOrder {
 export type PartitionComment = string[] | boolean | string
 
 export interface SortingNode {
+  hasMultipleImportDeclarations?: boolean
   dependencies?: string[]
   node: TSESTree.Node
   group?: string
