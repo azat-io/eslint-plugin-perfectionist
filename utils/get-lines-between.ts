105acc057725161b47870012dbd45c32cf98f025
Azat S. <to@azat.io>
2023-11-11 22:26:36 +0300

    feat: add partition by new line option to sort-objects rule

---
+++ utils/get-lines-between.ts
@@ -0,0 +1,16 @@
+import type { TSESLint } from '@typescript-eslint/utils'
+
+import type { SortingNode } from '../typings'
+
+export let getLinesBetween = (
+  source: TSESLint.SourceCode,
+  left: SortingNode,
+  right: SortingNode,
+) => {
+  let linesBetween = source.lines.slice(
+    left.node.loc.end.line,
+    right.node.loc.start.line - 1,
+  )
+
+  return linesBetween.filter(line => !line.trim().length).length
+}
