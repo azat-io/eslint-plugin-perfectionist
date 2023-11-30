4503614db91f03377eeb10d33280000c2681e519
Azat S. <to@azat.io>
2023-12-01 01:43:26 +0300

    docs: add font

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
+  site: 'https://eslint-plugin-perfectionist.azat.io',
   publicDir: './docs/public',
   compressHTML: true,
   srcDir: './docs',
