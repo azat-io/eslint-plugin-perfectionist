12abf66c38f3b04eaa957e700625ac77f1e2f2b2
Azat S. <to@azat.io>
2023-12-02 21:50:01 +0300

    docs: improve translations

--- docs/locales/en.ts
+++ docs/locales/en.ts
@@ -1,4 +1,7 @@
 export default {
+  slogan: 'Take Your Code to the Beauty Salon',
+  'view-on-github': 'View on GitHub',
+  'get-started': 'Get Started',
   configs: 'Configs',
   guide: 'Guide',
   rules: 'Rules',
--- docs/locales/ru.ts
+++ docs/locales/ru.ts
@@ -1,4 +1,7 @@
 export default {
+  slogan: 'Сводите Свой Код в Салон Красоты',
+  'view-on-github': 'Смотреть на GitHub',
+  'get-started': 'Начать Знакомство',
   configs: 'Конфигурация',
   guide: 'Руководство',
   rules: 'Правила',
