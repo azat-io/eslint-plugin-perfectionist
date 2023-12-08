f271c570589f0496508395c7085f4a823a158d03
Azat S. <to@azat.io>
2023-12-08 19:54:00 +0300

    chore: update astro to v4

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 
 export default defineConfig({
+  i18n: {
+    routing: {
+      prefixDefaultLocale: true,
+      strategy: 'pathname',
+    },
+    locales: ['en', 'ru'],
+    defaultLocale: 'en',
+  },
   site: 'https://eslint-plugin-perfectionist.azat.io',
   publicDir: './docs/public',
   integrations: [svelte()],
