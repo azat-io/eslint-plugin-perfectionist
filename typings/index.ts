d0571bc348270a995635448176fbae0f62e1e159
Azat S. <to@azat.io>
2024-03-09 22:55:13 +0300

    refactor: remove enum types in index file

--- typings/index.ts
+++ typings/index.ts
@@ -1,14 +1,3 @@
-export enum SortType {
-  'alphabetical' = 'alphabetical',
-  'line-length' = 'line-length',
-  'natural' = 'natural',
-}
-
-export enum SortOrder {
-  'desc' = 'desc',
-  'asc' = 'asc',
-}
-
 export type PartitionComment = string[] | boolean | string
 
 export interface SortingNode<Node extends unknown> {
