e9c20d0e9b20fb0b367517d289ad7dfc8dfaef56
Azat S. <to@azat.io>
2024-07-17 11:28:34 +0300

    test: fix test coverage

--- utils/get-source-code.ts
+++ utils/get-source-code.ts
@@ -1,5 +1,6 @@
 import type { TSESLint } from '@typescript-eslint/utils'
 
 export let getSourceCode = (context: TSESLint.RuleContext<string, unknown[]>) =>
+  /* v8 ignore next 2 */
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
   context.sourceCode ?? context.getSourceCode()
