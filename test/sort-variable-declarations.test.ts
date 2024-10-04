04f6bbfcf044bbd13c0d54ee862d1bc4a4191b36
hugo.prunaux <hugo.prunaux@gmail.com>
2024-10-05 01:26:25 +0200

    feat(sort-variable-declarations): adds possibility to trim/remove special characters

--- test/sort-variable-declarations.test.ts
+++ test/sort-variable-declarations.test.ts
@@ -872,6 +872,53 @@ describe(ruleName, () => {
         },
       )
     })
+
+    ruleTester.run(
+      `${ruleName}(${type}): allows to trim special characters`,
+      rule,
+      {
+        valid: [
+          {
+            code: dedent`
+              const
+                _a = 'a',
+                b = 'b',
+                _c = 'c'
+            `,
+            options: [
+              {
+                ...options,
+                specialCharacters: 'trim',
+              },
+            ],
+          },
+        ],
+        invalid: [],
+      },
+    )
+
+    ruleTester.run(
+      `${ruleName}(${type}): allows to remove special characters`,
+      rule,
+      {
+        valid: [
+          {
+            code: dedent`
+              const
+                ab = 'ab',
+                a_c = 'ac'
+            `,
+            options: [
+              {
+                ...options,
+                specialCharacters: 'remove',
+              },
+            ],
+          },
+        ],
+        invalid: [],
+      },
+    )
   })
 
   describe(`${ruleName}: sorting by natural order`, () => {
