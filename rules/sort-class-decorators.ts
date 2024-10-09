343d6d296a38326a9db618c47c37b82a43adb9cb
hugo.prunaux <hugo.prunaux@gmail.com>
2024-10-09 08:14:55 +0200

    feat: adds `specialCharacters` option

--- rules/sort-class-decorators.ts
+++ rules/sort-class-decorators.ts
@@ -30,6 +30,7 @@ export type Options<T extends string[]> = [
     customGroups: { [key in T[number]]: string[] | string }
     type: 'alphabetical' | 'line-length' | 'natural'
     partitionByComment: string[] | boolean | string
+    specialCharacters: 'remove' | 'trim' | 'keep'
     groups: (Group<T>[] | Group<T>)[]
     matcher: 'minimatch' | 'regex'
     order: 'desc' | 'asc'
@@ -71,6 +72,12 @@ export default createEslintRule<Options<string[]>, MESSAGE_ID>({
             description:
               'Controls whether sorting should be case-sensitive or not.',
             type: 'boolean',
+          },
+          specialCharacters: {
+            description:
+              'Controls how special characters should be handled before sorting.',
+            type: 'string',
+            enum: ['remove', 'trim', 'keep'],
           },
           partitionByComment: {
             description:
@@ -140,6 +147,7 @@ export default createEslintRule<Options<string[]>, MESSAGE_ID>({
       type: 'alphabetical',
       order: 'asc',
       ignoreCase: true,
+      specialCharacters: 'keep',
       partitionByComment: false,
       matcher: 'minimatch',
       groups: [],
@@ -173,6 +181,7 @@ export let sortDecorators = <MessageIds extends string>(
     type: 'alphabetical',
     matcher: 'minimatch',
     ignoreCase: true,
+    specialCharacters: 'keep',
     partitionByComment: false,
     customGroups: {},
     order: 'asc',
