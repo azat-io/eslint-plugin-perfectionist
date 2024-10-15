a7d3f8c20ee3f9806f7c29215b97c3327f67c754
Hugo <60015232+hugop95@users.noreply.github.com>
2024-10-15 10:28:29 +0200

    feat: add special characters and matcher to settings

---
+++ test/get-settings.test.ts
@@ -0,0 +1,38 @@
+import { describe, expect, it } from 'vitest'
+
+import type { Settings } from '../utils/get-settings'
+
+import { getSettings } from '../utils/get-settings'
+
+describe('get-settings', () => {
+  it('throws an error when an invalid setting is provided', () => {
+    expect(() => {
+      getSettings({
+        perfectionist: {
+          invalidOption1: 'value',
+          invalidOption2: 'value',
+        },
+      })
+    }).toThrow(
+      'Invalid Perfectionist setting(s): invalidOption1, invalidOption2',
+    )
+  })
+
+  it('accepts official settings provided', () => {
+    let allowedOptions: { [key in keyof Required<Settings>]: Settings[key] } = {
+      partitionByComment: 'value',
+      partitionByNewLine: true,
+      specialCharacters: 'keep',
+      ignorePattern: [],
+      ignoreCase: true,
+      matcher: 'regex',
+      order: 'asc',
+      type: 'alphabetical',
+    }
+    expect(() => {
+      getSettings({
+        perfectionist: allowedOptions,
+      })
+    }).not.toThrow()
+  })
+})
