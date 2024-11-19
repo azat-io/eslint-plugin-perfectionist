377f96ae468c7eb0b4e509640c4960d551b331c4
hugo.prunaux <hugo.prunaux@gmail.com>
2024-11-19 21:04:12 +0100

    feat: adds `get-node-decorators` function

---
+++ utils/get-node-decorators.ts
@@ -0,0 +1,15 @@
+import type { TSESTree } from '@typescript-eslint/types'
+
+type NodeWithDecorator = {
+  decorators: TSESTree.Decorator[]
+} & TSESTree.Node
+
+/*
+ * Projects without typescript-eslint parser will not have the `decorators`
+ * property on the node, so add a fallback.
+ */
+export let getNodeDecorators = (
+  node: NodeWithDecorator,
+  /* v8 ignore next 2 */
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+): TSESTree.Decorator[] => node.decorators ?? []
