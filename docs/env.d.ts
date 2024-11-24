ccb8baf60c8f1061f28b35850a869ee5bdf41334
Azat S. <to@azat.io>
2024-11-24 03:57:19 +0300

    chore: remove unused typings

--- docs/env.d.ts
+++ docs/env.d.ts
@@ -7,11 +7,3 @@ declare module '*.svg?raw' {
   let content: string
   export default content
 }
-
-declare module 'remark-sectionize' {
-  import type { Plugin } from 'unified'
-  import type { Root } from 'mdast'
-
-  let plugin: Plugin<object, Root>
-  export default plugin
-}
