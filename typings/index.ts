c739e3080a9d4893d813a6fac443107aa625f914
hugo.prunaux <hugo.prunaux@gmail.com>
2024-12-13 01:40:39 +0100

    refactor: extracts `sorting-node` into its own file

--- typings/index.ts
+++ typings/index.ts
@@ -1,11 +1,1 @@
-import type { TSESTree } from '@typescript-eslint/types'
-
-export interface SortingNode<Node extends TSESTree.Node = TSESTree.Node> {
-  hasMultipleImportDeclarations?: boolean
-  addSafetySemicolonWhenInline?: boolean
-  isEslintDisabled: boolean
-  group?: string
-  name: string
-  size: number
-  node: Node
-}
+export type { SortingNode } from './sorting-node'
---
+++ typings/sorting-node.ts
@@ -0,0 +1,11 @@
+import type { TSESTree } from '@typescript-eslint/types'
+
+export interface SortingNode<Node extends TSESTree.Node = TSESTree.Node> {
+  hasMultipleImportDeclarations?: boolean
+  addSafetySemicolonWhenInline?: boolean
+  isEslintDisabled: boolean
+  group?: string
+  name: string
+  size: number
+  node: Node
+}
