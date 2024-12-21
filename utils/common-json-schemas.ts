ee29c39afba8c0ea9ad59096426eebcd7b2b6068
hugo.prunaux <hugo.prunaux@gmail.com>
2024-12-21 23:15:16 +0100

    feat: updates common JSON schemas

--- utils/common-json-schemas.ts
+++ utils/common-json-schemas.ts
@@ -113,8 +113,7 @@ export let partitionByNewLineJsonSchema: JSONSchema4 = {
 }
 
 export let newlinesBetweenJsonSchema: JSONSchema4 = {
-  description:
-    'Specifies how new lines should be handled between modules members groups.',
+  description: 'Specifies how new lines should be handled between groups.',
   enum: ['ignore', 'always', 'never'],
   type: 'string',
 }
@@ -154,6 +153,33 @@ let customGroupNameJsonSchema: Record<string, JSONSchema4> = {
   },
 }
 
+let customGroupNewlinesInsideJsonSchema: Record<string, JSONSchema4> = {
+  newlinesInside: {
+    description:
+      'Specifies how new lines should be handled between members of the custom group.',
+    enum: ['always', 'never'],
+    type: 'string',
+  },
+}
+
+let customGroupNewlinesAboveJsonSchema: Record<string, JSONSchema4> = {
+  newlinesAbove: {
+    description:
+      'Specifies how new lines should be handled right above the custom group.',
+    enum: ['ignore', 'always', 'never'],
+    type: 'string',
+  },
+}
+
+let customGroupNewlinesBelowJsonSchema: Record<string, JSONSchema4> = {
+  newlinesBelow: {
+    description:
+      'Specifies how new lines should be handled right below the custom group.',
+    enum: ['ignore', 'always', 'never'],
+    type: 'string',
+  },
+}
+
 export let buildCustomGroupsArrayJsonSchema = ({
   singleCustomGroupJsonSchema,
 }: {
@@ -165,6 +191,9 @@ export let buildCustomGroupsArrayJsonSchema = ({
         properties: {
           ...customGroupNameJsonSchema,
           ...customGroupSortJsonSchema,
+          ...customGroupNewlinesAboveJsonSchema,
+          ...customGroupNewlinesInsideJsonSchema,
+          ...customGroupNewlinesBelowJsonSchema,
           anyOf: {
             items: {
               properties: {
@@ -185,6 +214,9 @@ export let buildCustomGroupsArrayJsonSchema = ({
         properties: {
           ...customGroupNameJsonSchema,
           ...customGroupSortJsonSchema,
+          ...customGroupNewlinesAboveJsonSchema,
+          ...customGroupNewlinesInsideJsonSchema,
+          ...customGroupNewlinesBelowJsonSchema,
           ...singleCustomGroupJsonSchema,
         },
         description: 'Custom group.',
