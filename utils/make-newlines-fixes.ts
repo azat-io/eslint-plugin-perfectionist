01968fd58a052cffa38446164a539d834054edef
hugo.prunaux <hugo.prunaux@gmail.com>
2024-12-22 00:38:46 +0100

    feat: fixes `customGroups.newlinesInside` error

--- utils/make-newlines-fixes.ts
+++ utils/make-newlines-fixes.ts
@@ -2,19 +2,27 @@ import type { TSESLint } from '@typescript-eslint/utils'
 
 import type { SortingNode } from '../types/sorting-node'
 
+import { getNewlinesBetweenOption } from './get-newlines-between-option'
 import { getLinesBetween } from './get-lines-between'
-import { getGroupNumber } from './get-group-number'
 import { getNodeRange } from './get-node-range'
 
 interface MakeNewlinesFixesParameters {
-  options: {
-    newlinesBetween: 'ignore' | 'always' | 'never'
-    groups: (string[] | string)[]
-  }
   sourceCode: TSESLint.SourceCode
   sortedNodes: SortingNode[]
   fixer: TSESLint.RuleFixer
   nodes: SortingNode[]
+  options: Options
+}
+
+interface Options {
+  customGroups?: Record<string, string[] | string> | CustomGroup[]
+  newlinesBetween: 'ignore' | 'always' | 'never'
+  groups: (string[] | string)[]
+}
+
+interface CustomGroup {
+  newlinesInside?: 'always' | 'never'
+  groupName: string
 }
 
 export let makeNewlinesFixes = ({
@@ -24,10 +32,6 @@ export let makeNewlinesFixes = ({
   fixer,
   nodes,
 }: MakeNewlinesFixesParameters): TSESLint.RuleFix[] => {
-  if (options.newlinesBetween === 'ignore') {
-    return []
-  }
-
   let fixes: TSESLint.RuleFix[] = []
 
   for (let i = 0; i < sortedNodes.length - 1; i++) {
@@ -36,11 +40,16 @@ export let makeNewlinesFixes = ({
     let sortedSortingNode = sortedNodes.at(i)!
     let nextSortedSortingNode = sortedNodes.at(i + 1)!
 
-    let nodeGroupNumber = getGroupNumber(options.groups, sortedSortingNode)
-    let nextNodeGroupNumber = getGroupNumber(
-      options.groups,
-      nextSortedSortingNode,
-    )
+    let newlinesBetween = getNewlinesBetweenOption({
+      nextSortingNode: nextSortedSortingNode,
+      sortingNode: sortedSortingNode,
+      options,
+    })
+
+    if (newlinesBetween === 'ignore') {
+      continue
+    }
+
     let currentNodeRange = getNodeRange({
       node: sortingNode.node,
       sourceCode,
@@ -65,20 +74,11 @@ export let makeNewlinesFixes = ({
     )
 
     let rangeReplacement: undefined | string
-    if (
-      (options.newlinesBetween === 'always' &&
-        nodeGroupNumber === nextNodeGroupNumber &&
-        linesBetweenMembers !== 0) ||
-      (options.newlinesBetween === 'never' && linesBetweenMembers > 0)
-    ) {
+    if (newlinesBetween === 'never' && linesBetweenMembers !== 0) {
       rangeReplacement = getStringWithoutInvalidNewlines(textBetweenNodes)
     }
 
-    if (
-      options.newlinesBetween === 'always' &&
-      nodeGroupNumber !== nextNodeGroupNumber &&
-      linesBetweenMembers !== 1
-    ) {
+    if (newlinesBetween === 'always' && linesBetweenMembers !== 1) {
       rangeReplacement = addNewlineBeforeFirstNewline(
         linesBetweenMembers > 1
           ? getStringWithoutInvalidNewlines(textBetweenNodes)
