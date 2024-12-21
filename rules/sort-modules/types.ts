a03a5ae2e989e68e13106ebf667e27105e407e7c
hugo.prunaux <hugo.prunaux@gmail.com>
2024-12-22 00:53:07 +0100

    feat(sort-modules): adds `customGroups` newlines-related options

--- rules/sort-modules/types.ts
+++ rules/sort-modules/types.ts
@@ -67,7 +67,12 @@ interface AllowedModifiersPerSelector {
   type: DeclareModifier | ExportModifier
 }
 
-type CustomGroup = (
+type CustomGroup = {
+  newlinesAbove?: 'ignore' | 'always' | 'never'
+  newlinesBelow?: 'ignore' | 'always' | 'never'
+  newlinesInside?: 'always' | 'never'
+  groupName: string
+} & (
   | {
       order?: SortModulesOptions[0]['order']
       type?: SortModulesOptions[0]['type']
@@ -76,9 +81,7 @@ type CustomGroup = (
       type?: 'unsorted'
     }
 ) &
-  (SingleCustomGroup | AnyOfCustomGroup) & {
-    groupName: string
-  }
+  (SingleCustomGroup | AnyOfCustomGroup)
 /**
  * Only used in code, so I don't know if it's worth maintaining this.
  */
