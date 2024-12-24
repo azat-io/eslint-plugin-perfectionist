63ae205fec9bda5b683024fd09d964e1f61bce40
hugo.prunaux <hugo.prunaux@gmail.com>
2024-12-24 16:06:51 +0100

    feat: implements `block` and `line` partition comment check

--- utils/is-partition-comment.ts
+++ utils/is-partition-comment.ts
@@ -1,5 +1,7 @@
 import type { TSESTree } from '@typescript-eslint/types'
 
+import { AST_TOKEN_TYPES } from '@typescript-eslint/types'
+
 import { getEslintDisabledRules } from './get-eslint-disabled-rules'
 import { matches } from './matches'
 
@@ -22,18 +24,49 @@ export let isPartitionComment = ({
   if (getEslintDisabledRules(comment.value) || !partitionByComment) {
     return false
   }
+
   let trimmedComment = comment.value.trim()
+
+  if (
+    Array.isArray(partitionByComment) ||
+    typeof partitionByComment === 'boolean' ||
+    typeof partitionByComment === 'string'
+  ) {
+    return isTrimmedCommentPartitionComment({
+      partitionByComment,
+      trimmedComment,
+    })
+  }
+
+  let relevantPartitionByComment =
+    comment.type === AST_TOKEN_TYPES.Block
+      ? partitionByComment.block
+      : partitionByComment.line
+
+  return (
+    // eslint-disable-next-line no-undefined
+    relevantPartitionByComment !== undefined &&
+    isTrimmedCommentPartitionComment({
+      partitionByComment: relevantPartitionByComment,
+      trimmedComment,
+    })
+  )
+}
+
+let isTrimmedCommentPartitionComment = ({
+  partitionByComment,
+  trimmedComment,
+}: {
+  partitionByComment: string[] | boolean | string
+  trimmedComment: string
+}): boolean => {
   if (typeof partitionByComment === 'boolean') {
     return partitionByComment
   }
   if (typeof partitionByComment === 'string') {
     return matches(trimmedComment.trim(), partitionByComment)
   }
-  if (Array.isArray(partitionByComment)) {
-    return partitionByComment.some(pattern =>
-      matches(trimmedComment.trim(), pattern),
-    )
-  }
-  // TODO - implement block and line check
-  throw new Error('not implemented')
+  return partitionByComment.some(pattern =>
+    matches(trimmedComment.trim(), pattern),
+  )
 }
