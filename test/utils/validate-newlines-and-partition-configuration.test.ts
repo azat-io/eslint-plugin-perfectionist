a5fa7df55e61b9a28082f3cf192ea1e70df22adf
hugo.prunaux <hugo.prunaux@gmail.com>
2024-12-26 17:18:09 +0100

    test: fixes invalid test naming

--- test/utils/validate-newlines-and-partition-configuration.test.ts
+++ test/utils/validate-newlines-and-partition-configuration.test.ts
@@ -3,41 +3,31 @@ import { describe, expect, it } from 'vitest'
 import { validateNewlinesAndPartitionConfiguration } from '../../utils/validate-newlines-and-partition-configuration'
 
 describe('validate-newlines-and-partition-configuration', () => {
-  let partitionByCommentValues: (string[] | boolean | string)[] = [
-    true,
-    'partitionComment',
-    ['partition1', 'partition2'],
-  ]
-
-  it("throws an error when 'partitionComment' is enabled and 'newlinesBetween' is not 'ignore'", () => {
+  it("throws an error when 'partitionByNewline' is enabled and 'newlinesBetween' is not 'ignore'", () => {
     let newlinesBetweenValues = ['always', 'never'] as const
 
     for (let newlinesBetween of newlinesBetweenValues) {
-      for (let partitionByComment of partitionByCommentValues) {
-        expect(() => {
-          validateNewlinesAndPartitionConfiguration({
-            partitionByNewLine: partitionByComment,
-            newlinesBetween,
-          })
-        }).toThrow(
-          "The 'partitionByNewLine' and 'newlinesBetween' options cannot be used together",
-        )
-      }
-    }
-  })
-
-  it("allows 'partitionComment' when 'newlinesBetween' is 'ignore'", () => {
-    for (let partitionByComment of partitionByCommentValues) {
       expect(() => {
         validateNewlinesAndPartitionConfiguration({
-          partitionByNewLine: partitionByComment,
-          newlinesBetween: 'ignore',
+          partitionByNewLine: true,
+          newlinesBetween,
         })
-      }).not.toThrow()
+      }).toThrow(
+        "The 'partitionByNewLine' and 'newlinesBetween' options cannot be used together",
+      )
     }
   })
 
-  it("allows 'newlinesBetween' when 'partitionByComment' is 'false'", () => {
+  it("allows 'partitionByNewline' when 'newlinesBetween' is 'ignore'", () => {
+    expect(() => {
+      validateNewlinesAndPartitionConfiguration({
+        newlinesBetween: 'ignore',
+        partitionByNewLine: true,
+      })
+    }).not.toThrow()
+  })
+
+  it("allows 'newlinesBetween' when 'partitionByNewline' is 'false'", () => {
     let newlinesBetweenValues = ['always', 'never', 'ignore'] as const
 
     for (let newlinesBetween of newlinesBetweenValues) {
