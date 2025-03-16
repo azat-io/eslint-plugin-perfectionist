2b27b7d3c318ecd19fb8f5cc197dc6b4c667b77a
Hugo <60015232+hugop95@users.noreply.github.com>
2025-03-16 12:18:30 +0100

    fix: throw an error if regexp expressions are entered

--- utils/matches.ts
+++ utils/matches.ts
@@ -9,5 +9,14 @@ export let matches = (value: string, regexOption: RegexOption): boolean => {
     return new RegExp(regexOption).test(value)
   }
 
+  // Handler for non-string regexes until an error is thrown on the JSON schema
+  // Level
+  if ('source' in regexOption) {
+    throw new Error(
+      'Invalid configuration: please enter your RegExp expressions as strings.\n' +
+        'For example, write ".*foo" instead of /.*foo/',
+    )
+  }
+
   return new RegExp(regexOption.pattern, regexOption.flags).test(value)
 }
