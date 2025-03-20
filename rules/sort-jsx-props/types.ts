248b8e124bfd02526b4e3e58abd35c34d3128ff7
hugo.prunaux <hugo.prunaux@gmail.com>
2025-03-20 14:38:50 +0100

    refactor: use new API for `groups` (1/2)
    - Create types.

--- rules/sort-jsx-props/types.ts
+++ rules/sort-jsx-props/types.ts
@@ -5,6 +5,7 @@ import type {
   GroupsOptions,
   RegexOption,
 } from '../../types/common-options'
+import type { JoinWithDash } from '../../types/join-with-dash'
 
 export type Options = Partial<
   {
@@ -23,4 +24,57 @@ export type Options = Partial<
   } & CommonOptions
 >[]
 
-type Group = 'multiline' | 'shorthand' | 'unknown' | string
+export interface SingleCustomGroup {
+  elementValuePattern?: RegexOption
+  elementNamePattern?: RegexOption
+  modifiers?: Modifier[]
+  selector?: Selector
+}
+
+export type Selector = MultilineSelector | ShorthandSelector | PropertySelector
+
+export type Modifier = MultilineModifier | ShorthandModifier
+
+type PropertyGroup = JoinWithDash<
+  [ShorthandModifier, MultilineModifier, PropertySelector]
+>
+
+/**
+ * Only used in code, so I don't know if it's worth maintaining this.
+ */
+type Group =
+  | ShorthandGroup
+  | MultilineGroup
+  | PropertyGroup
+  | 'unknown'
+  | string
+
+/**
+ * @deprecated For {@link `MultilineModifier`}
+ */
+type MultilineGroup = JoinWithDash<[MultilineSelector]>
+
+/**
+ * @deprecated For {@link `ShorthandModifier`}
+ */
+type ShorthandGroup = JoinWithDash<[ShorthandSelector]>
+
+/**
+ * @deprecated For {@link `ShorthandModifier`}
+ */
+type ShorthandSelector = 'shorthand'
+
+/**
+ * @deprecated For {@link `MultilineModifier`}
+ */
+type MultilineSelector = 'multiline'
+
+type MultilineModifier = 'multiline'
+
+type ShorthandModifier = 'shorthand'
+
+type PropertySelector = 'prop'
+
+export let allSelectors: Selector[] = ['multiline', 'prop', 'shorthand']
+
+export let allModifiers: Modifier[] = ['shorthand', 'multiline']
