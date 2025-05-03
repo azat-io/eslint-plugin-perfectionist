97770fad7a52fbeaf5073998d04b18efb89414b6
hugo.prunaux <hugo.prunaux@gmail.com>
2025-05-03 22:12:17 +0200

    refactor: simplify `readClosestTsConfigByPath`

--- rules/sort-imports/read-closest-ts-config-by-path.ts
+++ rules/sort-imports/read-closest-ts-config-by-path.ts
@@ -15,27 +15,27 @@ export interface ReadClosestTsConfigByPathValue {
   cache: ts.ModuleResolutionCache
 }
 
-interface ReadClosestTsConfigByPathParameters {
-  tsconfigRootDir: string
-  contextCwd: string
-  filePath: string
-}
-
 export let directoryCacheByPath = new Map<string, string>()
 export let contentCacheByPath = new Map<
   string,
   ReadClosestTsConfigByPathValue
 >()
 
-export let readClosestTsConfigByPath = (
-  input: ReadClosestTsConfigByPathParameters,
-): ReadClosestTsConfigByPathValue | null => {
+export let readClosestTsConfigByPath = ({
+  tsconfigRootDir,
+  contextCwd,
+  filePath,
+}: {
+  tsconfigRootDir: string
+  contextCwd: string
+  filePath: string
+}): ReadClosestTsConfigByPathValue | null => {
   let typescriptImport = getTypescriptImport()
   if (!typescriptImport) {
     return null
   }
 
-  let directory = path.dirname(input.filePath)
+  let directory = path.dirname(filePath)
   let checkedDirectories = [directory]
 
   do {
@@ -49,22 +49,15 @@ export let readClosestTsConfigByPath = (
       for (let checkedDirectory of checkedDirectories) {
         directoryCacheByPath.set(checkedDirectory, cachedDirectory)
       }
-      return getCompilerOptions(
-        typescriptImport,
-        input.contextCwd,
-        cachedDirectory,
-      )
+      return getCompilerOptions(typescriptImport, contextCwd, cachedDirectory)
     }
 
     directory = path.dirname(directory)
     checkedDirectories.push(directory)
-  } while (
-    directory.length > 1 &&
-    directory.length >= input.tsconfigRootDir.length
-  )
+  } while (directory.length > 1 && directory.length >= tsconfigRootDir.length)
 
   throw new Error(
-    `Couldn't find any tsconfig.json relative to '${input.filePath}' within '${input.tsconfigRootDir}'.`,
+    `Couldn't find any tsconfig.json relative to '${filePath}' within '${tsconfigRootDir}'.`,
   )
 }
 
