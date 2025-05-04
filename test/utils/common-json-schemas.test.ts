a632794e84b58148d6a2de6bacfd43253ff79ef9
hugo.prunaux <hugo.prunaux@gmail.com>
2025-05-04 17:29:47 +0200

    test: add tests

---
+++ test/utils/common-json-schemas.test.ts
@@ -0,0 +1,297 @@
+import { describe, expect, it } from 'vitest'
+import Ajv from 'ajv-draft-04'
+
+import {
+  buildCustomGroupsArrayJsonSchema,
+  partitionByCommentJsonSchema,
+  newlinesBetweenJsonSchema,
+  commonJsonSchemas,
+  groupsJsonSchema,
+  regexJsonSchema,
+} from '../../utils/common-json-schemas'
+
+describe('common-json-schemas', () => {
+  describe('commonJsonSchemas', () => {
+    let commonJsonSchemaValidator = new Ajv().compile({
+      properties: commonJsonSchemas,
+      additionalProperties: false,
+      type: 'object',
+    })
+
+    describe('type', () => {
+      it.each(['alphabetical', 'natural', 'line-length', 'custom', 'unsorted'])(
+        "should allow' %s'",
+        type => {
+          expect(
+            commonJsonSchemaValidator({
+              type,
+            }),
+          ).toBeTruthy()
+        },
+      )
+
+      it('should not allow invalid values', () => {
+        expect(
+          commonJsonSchemaValidator({
+            type: 'invalid',
+          }),
+        ).toBeFalsy()
+      })
+    })
+
+    describe('order', () => {
+      it.each(['asc', 'desc'])("should allow' %s'", order => {
+        expect(
+          commonJsonSchemaValidator({
+            order,
+          }),
+        ).toBeTruthy()
+      })
+
+      it('should not allow invalid values', () => {
+        expect(
+          commonJsonSchemaValidator({
+            order: 'invalid',
+          }),
+        ).toBeFalsy()
+      })
+    })
+
+    describe('specialCharacters', () => {
+      it.each(['remove', 'trim', 'keep'])(
+        "should allow' %s'",
+        specialCharacters => {
+          expect(
+            commonJsonSchemaValidator({
+              specialCharacters,
+            }),
+          ).toBeTruthy()
+        },
+      )
+
+      it('should not allow invalid values', () => {
+        expect(
+          commonJsonSchemaValidator({
+            specialCharacters: 'invalid',
+          }),
+        ).toBeFalsy()
+      })
+    })
+
+    describe('fallbackSort', () => {
+      describe('type', () => {
+        it.each([
+          'alphabetical',
+          'natural',
+          'line-length',
+          'custom',
+          'unsorted',
+        ])("should allow' %s'", type => {
+          expect(
+            commonJsonSchemaValidator({
+              fallbackSort: {
+                type,
+              },
+            }),
+          ).toBeTruthy()
+        })
+
+        it('should not allow invalid values', () => {
+          expect(
+            commonJsonSchemaValidator({
+              fallbackSort: {
+                type: 'invalid',
+              },
+            }),
+          ).toBeFalsy()
+        })
+      })
+
+      describe('order', () => {
+        it.each(['asc', 'desc'])("should allow' %s'", order => {
+          expect(
+            commonJsonSchemaValidator({
+              fallbackSort: {
+                order,
+              },
+            }),
+          ).toBeTruthy()
+        })
+
+        it('should not allow invalid values', () => {
+          expect(
+            commonJsonSchemaValidator({
+              fallbackSort: {
+                order: 'invalid',
+              },
+            }),
+          ).toBeFalsy()
+        })
+      })
+
+      it('should not allow additional properties', () => {
+        expect(
+          commonJsonSchemaValidator({
+            fallbackSort: {
+              somethingElse: 'something',
+            },
+          }),
+        ).toBeFalsy()
+      })
+    })
+  })
+
+  describe('newlinesBetween', () => {
+    let newlinesBetweenJsonSchemaValidator = new Ajv().compile(
+      newlinesBetweenJsonSchema,
+    )
+
+    it.each(['ignore', 'always', 'never'])(
+      "should allow' %s'",
+      newlinesBetween => {
+        expect(newlinesBetweenJsonSchemaValidator(newlinesBetween)).toBeTruthy()
+      },
+    )
+
+    it('should not allow invalid values', () => {
+      expect(newlinesBetweenJsonSchemaValidator('invalid')).toBeFalsy()
+    })
+  })
+
+  describe('groups', () => {
+    let groupsJsonSchemaValidator = new Ajv().compile(groupsJsonSchema)
+
+    it('should allow an array of strings', () => {
+      expect(groupsJsonSchemaValidator(['group1', 'group2'])).toBeTruthy()
+    })
+
+    it('should allow an array of arrays of strings', () => {
+      expect(
+        groupsJsonSchemaValidator([['group1', 'group2'], ['group3']]),
+      ).toBeTruthy()
+    })
+
+    describe('newlinesBetween', () => {
+      it.each(['ignore', 'always', 'never'])(
+        "should allow' %s'",
+        newlinesBetween => {
+          expect(
+            groupsJsonSchemaValidator([
+              'group1',
+              { newlinesBetween },
+              'group2',
+            ]),
+          ).toBeTruthy()
+        },
+      )
+
+      it('should not allow invalid values', () => {
+        expect(
+          groupsJsonSchemaValidator([
+            'group1',
+            { newlinesBetween: 'invalid' },
+            'group2',
+          ]),
+        ).toBeFalsy()
+      })
+
+      it('should not allow additional properties', () => {
+        expect(
+          groupsJsonSchemaValidator([
+            'group1',
+            { newlinesBetween: 'always', something: 'something' },
+            'group2',
+          ]),
+        ).toBeFalsy()
+      })
+    })
+  })
+
+  describe('customGroups', () => {
+    let customGroupsJsonSchema = new Ajv().compile(
+      buildCustomGroupsArrayJsonSchema({
+        singleCustomGroupJsonSchema: {
+          customGroupProperty: { type: 'string' },
+        },
+      }),
+    )
+
+    it('should allow arrays of custom groups', () => {
+      expect(
+        customGroupsJsonSchema([
+          {
+            customGroupProperty: 'value',
+            groupName: 'group',
+          },
+        ]),
+      ).toBeTruthy()
+    })
+
+    it("should allow arrays of 'anyOf' custom groups", () => {
+      expect(
+        customGroupsJsonSchema([
+          {
+            anyOf: [
+              {
+                customGroupProperty: 'ss',
+              },
+            ],
+            groupName: 'group',
+          },
+        ]),
+      ).toBeTruthy()
+    })
+  })
+
+  describe('partitionByComment', () => {
+    let partitionByCommentJsonSchemaValidator = new Ajv().compile(
+      partitionByCommentJsonSchema,
+    )
+
+    it('should allow boolean values', () => {
+      expect(partitionByCommentJsonSchemaValidator(true)).toBeTruthy()
+    })
+
+    it.each([{ block: true }, { line: true }, { block: true, line: true }])(
+      "should allow '%s'",
+      partitionByComment => {
+        expect(
+          partitionByCommentJsonSchemaValidator(partitionByComment),
+        ).toBeTruthy()
+      },
+    )
+  })
+
+  describe('regex', () => {
+    let regexJsonSchemaValidator = new Ajv().compile(regexJsonSchema)
+
+    it('should allow string values', () => {
+      expect(regexJsonSchemaValidator('some string')).toBeTruthy()
+    })
+
+    it.each([{ pattern: 'pattern' }, { pattern: 'pattern', flags: 'flags' }])(
+      "should allow '%s'",
+      regex => {
+        expect(regexJsonSchemaValidator(regex)).toBeTruthy()
+      },
+    )
+
+    it("should enforce 'pattern'", () => {
+      expect(
+        regexJsonSchemaValidator({
+          something: 'something',
+          pattern: 'pattern',
+        }),
+      ).toBeFalsy()
+    })
+
+    it('should not allow additional properties', () => {
+      expect(
+        regexJsonSchemaValidator({
+          something: 'something',
+          pattern: 'pattern',
+        }),
+      ).toBeFalsy()
+    })
+  })
+})
