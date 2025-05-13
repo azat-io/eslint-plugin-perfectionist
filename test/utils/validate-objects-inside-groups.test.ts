f249548b9962885ae3c9bfa0e37e648ceb4ed4e2
hugo.prunaux <hugo.prunaux@gmail.com>
2025-05-13 13:55:06 +0200

    refactor: update validation (1/2)
    - Simply rename files

--- test/utils/validate-objects-inside-groups.test.ts
+++ test/utils/validate-objects-inside-groups.test.ts
@@ -1,11 +1,11 @@
 import { describe, expect, it } from 'vitest'
 
-import { validateNewlinesBetweenInsideGroups } from '../../utils/validate-newlines-between-inside-groups'
+import { validateObjectsInsideGroups } from '../../utils/validate-objects-inside-groups'
 
-describe('validate-newlines-between-inside-groups', () => {
+describe('validate-objects-inside-groups', () => {
   it('throws an error with consecutive newlines objects', () => {
     expect(() => {
-      validateNewlinesBetweenInsideGroups({
+      validateObjectsInsideGroups({
         groups: [{ newlinesBetween: 'always' }, { newlinesBetween: 'always' }],
       })
     }).toThrow("Consecutive 'newlinesBetween' objects are not allowed")
