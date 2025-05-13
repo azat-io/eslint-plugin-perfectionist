ce0e43977aa81e69afbd92abbcbc336445b9c97d
hugo.prunaux <hugo.prunaux@gmail.com>
2025-05-13 13:55:54 +0200

    feat: update validation (2/2)

--- test/utils/validate-objects-inside-groups.test.ts
+++ test/utils/validate-objects-inside-groups.test.ts
@@ -3,11 +3,41 @@ import { describe, expect, it } from 'vitest'
 import { validateObjectsInsideGroups } from '../../utils/validate-objects-inside-groups'
 
 describe('validate-objects-inside-groups', () => {
-  it('throws an error with consecutive newlines objects', () => {
-    expect(() => {
-      validateObjectsInsideGroups({
-        groups: [{ newlinesBetween: 'always' }, { newlinesBetween: 'always' }],
-      })
-    }).toThrow("Consecutive 'newlinesBetween' objects are not allowed")
-  })
+  it.each(buildAllPossibleCases())(
+    'throws an error with consecutive objects-based elements (%s)',
+    ({ second, first }) => {
+      expect(() => {
+        validateObjectsInsideGroups({
+          groups: [first, second],
+        })
+      }).toThrow(
+        'Consecutive objects (`newlinesBetween` or `commentAbove` are not allowed: merge them into a single object',
+      )
+    },
+  )
 })
+
+let buildAllPossibleCases = (): {
+  second: (typeof cases)[number]
+  first: (typeof cases)[number]
+}[] => {
+  let cases = [
+    { newlinesBetween: 'always' },
+    { commentAbove: 'comment' },
+    { newlinesBetween: 'always', commentAbove: 'comment' },
+  ] as const
+
+  let returnValue: {
+    second: (typeof cases)[number]
+    first: (typeof cases)[number]
+  }[] = []
+  for (let item of cases) {
+    for (let item1 of cases) {
+      returnValue.push({
+        second: item1,
+        first: item,
+      })
+    }
+  }
+  return returnValue
+}
