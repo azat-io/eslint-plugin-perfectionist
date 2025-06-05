c8f0e875e3472fb107585428bf8915a31bd49ba9
hugo.prunaux <hugo.prunaux@gmail.com>
2025-06-05 02:35:00 +0200

    feat: allow `0` and `1` to be passed
    - ℹ️ Hide whitespace changes for this commit.

--- utils/get-newlines-between-option.ts
+++ utils/get-newlines-between-option.ts
@@ -94,16 +94,17 @@ export let getNewlinesBetweenOption = ({
       let newlinesBetweenOptions = new Set(
         groupsWithAllNewlinesBetween
           .filter(isNewlinesBetweenOption)
-          .map(group => group.newlinesBetween),
+          .map(group => group.newlinesBetween)
+          .map(convertNewlinesBetweenOptionToNumber),
       )
 
-      if (newlinesBetweenOptions.has('always')) {
+      if (newlinesBetweenOptions.has(1)) {
         return 1
       }
       if (newlinesBetweenOptions.has('ignore')) {
         return 'ignore'
       }
-      if (newlinesBetweenOptions.has('never')) {
+      if (newlinesBetweenOptions.has(0)) {
         return 0
       }
     }
@@ -121,10 +122,13 @@ let getGlobalNewlinesBetweenOption = ({
   nextNodeGroupIndex: number
   nodeGroupIndex: number
 }): 'ignore' | 0 | 1 => {
-  if (newlinesBetween === 'ignore') {
+  let numberNewlinesBetween =
+    convertNewlinesBetweenOptionToNumber(newlinesBetween)
+
+  if (numberNewlinesBetween === 'ignore') {
     return 'ignore'
   }
-  if (newlinesBetween === 'never') {
+  if (numberNewlinesBetween === 0) {
     return 0
   }
   return nodeGroupIndex === nextNodeGroupIndex ? 'never' : 'always'
@@ -134,9 +138,6 @@ let buildGroupsWithAllNewlinesBetween = (
   groups: GroupsOptions<string>,
   globalNewlinesBetweenOption: 'ignore' | 0 | 1,
 ): GroupsOptions<string> => {
-  let convertedGlobalNewlinesBetweenOptionNumber =
-    convertNewlinesBetweenNumberToOption(globalNewlinesBetweenOption)
-
   let returnValue: GroupsOptions<string> = []
   for (let i = 0; i < groups.length; i++) {
     let group = groups[i]!
@@ -145,7 +146,7 @@ let buildGroupsWithAllNewlinesBetween = (
       let previousGroup = groups[i - 1]
       if (previousGroup && !isNewlinesBetweenOption(previousGroup)) {
         returnValue.push({
-          newlinesBetween: convertedGlobalNewlinesBetweenOptionNumber,
+          newlinesBetween: globalNewlinesBetweenOption,
         })
       }
     }
@@ -155,21 +156,6 @@ let buildGroupsWithAllNewlinesBetween = (
   return returnValue
 }
 
-let convertNewlinesBetweenNumberToOption = (
-  newlinesBetween: 'ignore' | 0 | 1,
-): NewlinesBetweenOption => {
-  switch (newlinesBetween) {
-    case 'ignore':
-      return 'ignore'
-    case 0:
-      return 'never'
-    case 1:
-      return 'always'
-    default:
-      throw new UnreachableCaseError(newlinesBetween)
-  }
-}
-
 let convertNewlinesBetweenOptionToNumber = (
   newlinesBetween: NewlinesBetweenOption,
 ): 'ignore' | 0 | 1 => {
@@ -177,8 +163,10 @@ let convertNewlinesBetweenOptionToNumber = (
     case 'ignore':
       return 'ignore'
     case 'always':
+    case 1:
       return 1
     case 'never':
+    case 0:
       return 0
     /* v8 ignore next 2 */
     default:
