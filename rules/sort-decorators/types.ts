0d7c6c29804426b434ff3730ae29f94a9213e70e
Hugo <60015232+hugop95@users.noreply.github.com>
2025-09-10 21:50:26 +0200

    feat(sort-decorators): add array-based custom groups api

---
+++ rules/sort-decorators/types.ts
@@ -0,0 +1,44 @@
+import type { JSONSchema4 } from '@typescript-eslint/utils/json-schema'
+import type { TSESTree } from '@typescript-eslint/types'
+
+import type {
+  DeprecatedCustomGroupsOption,
+  PartitionByCommentOption,
+  NewlinesBetweenOption,
+  CustomGroupsOption,
+  CommonOptions,
+  GroupsOptions,
+  RegexOption,
+} from '../../types/common-options'
+import type { SortingNode } from '../../types/sorting-node'
+
+import { regexJsonSchema } from '../../utils/common-json-schemas'
+
+export type Options = Partial<
+  {
+    customGroups:
+      | CustomGroupsOption<SingleCustomGroup>
+      | DeprecatedCustomGroupsOption
+    partitionByComment: PartitionByCommentOption
+    newlinesBetween: NewlinesBetweenOption
+    groups: GroupsOptions<Group>
+    partitionByNewLine: boolean
+    sortOnParameters: boolean
+    sortOnProperties: boolean
+    sortOnAccessors: boolean
+    sortOnMethods: boolean
+    sortOnClasses: boolean
+  } & CommonOptions
+>[]
+
+export interface SingleCustomGroup {
+  elementNamePattern?: RegexOption
+}
+
+export type SortDecoratorsSortingNode = SortingNode<TSESTree.Decorator>
+
+type Group = 'unknown' | string
+
+export let singleCustomGroupJsonSchema: Record<string, JSONSchema4> = {
+  elementNamePattern: regexJsonSchema,
+}
