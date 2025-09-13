4988fce2af1f9d1270ced2c031faaf4561fab871
Hugo <60015232+hugop95@users.noreply.github.com>
2025-09-13 10:48:12 +0200

    feat(sort-heritage-clauses): add array-based custom groups api

---
+++ rules/sort-heritage-clauses/types.ts
@@ -0,0 +1,35 @@
+import type { JSONSchema4 } from '@typescript-eslint/utils/json-schema'
+
+import type {
+  DeprecatedCustomGroupsOption,
+  PartitionByCommentOption,
+  NewlinesBetweenOption,
+  CustomGroupsOption,
+  CommonOptions,
+  GroupsOptions,
+  RegexOption,
+} from '../../types/common-options'
+
+import { regexJsonSchema } from '../../utils/common-json-schemas'
+
+export type Options = Partial<
+  {
+    customGroups:
+      | CustomGroupsOption<SingleCustomGroup>
+      | DeprecatedCustomGroupsOption
+    partitionByComment: PartitionByCommentOption
+    newlinesBetween: NewlinesBetweenOption
+    groups: GroupsOptions<Group>
+    partitionByNewLine: boolean
+  } & CommonOptions
+>[]
+
+export interface SingleCustomGroup {
+  elementNamePattern?: RegexOption
+}
+
+type Group = 'unknown' | string
+
+export let singleCustomGroupJsonSchema: Record<string, JSONSchema4> = {
+  elementNamePattern: regexJsonSchema,
+}
