c744e58df10031192f61d207fa6f6abe7f5fa7dc
hugo.prunaux <hugo.prunaux@gmail.com>
2025-09-16 23:34:38 +0200

    fix: fix NaN values being considered
    - `NaN | 42` is actually `42`, so an invalid/unparsable expression could result in a numeric enum.

--- test/rules/sort-enums.test.ts
+++ test/rules/sort-enums.test.ts
@@ -4925,6 +4925,22 @@ describe('sort-enums', () => {
           },
         ],
       })
+
+      await valid({
+        code: dedent`
+          enum Enum {
+              A = 1,
+              B = A,
+              C = -A,
+              D = 2,
+            }
+        `,
+        options: [
+          {
+            forceNumericSort: true,
+          },
+        ],
+      })
     })
 
     it.each(['alphabetical', 'line-length', 'natural'])(
