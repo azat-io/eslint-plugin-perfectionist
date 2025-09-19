4b0a8237a82bd4c7cbb61fb1d347a744bbb56078
Azat S. <to@azat.io>
2025-09-19 22:22:00 +0300

    chore: update dependencies

--- rules/sort-switch-case.ts
+++ rules/sort-switch-case.ts
@@ -210,7 +210,7 @@ export default createEslintRule<Options, MessageId>({
       let sortedSortingNodeGroupsForBlockSort = [
         ...sortingNodeGroupsForBlockSort,
       ]
-        .sort((a, b) => {
+        .toSorted((a, b) => {
           if (lastBlockCaseShouldStayInPlace) {
             if (a === lastNodeGroup) {
               return 1
