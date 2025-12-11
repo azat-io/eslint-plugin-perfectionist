84e2871f3dbeaab24417eb24ce7be6494d4cef93
hugo.prunaux <hugo.prunaux@gmail.com>
2025-12-11 22:38:33 +0100

    feat: add `scope` support

---
+++ rules/sort-object-types/partition-patterns-by-scope.ts
@@ -0,0 +1,3 @@
+import { partitionPatternsByScope as sortObjectsPartitionPatternsByScope } from '../sort-objects/partition-patterns-by-scope'
+
+export let partitionPatternsByScope = sortObjectsPartitionPatternsByScope
--- rules/sort-object-types/passes-declaration-matches-pattern-filter.ts
+++ rules/sort-object-types/passes-declaration-matches-pattern-filter.ts
@@ -4,7 +4,9 @@ import type {
   ObjectTypeParentForDeclarationMatch,
   ScopedRegexOption,
 } from './types'
+import type { SingleRegexOption } from '../sort-objects/partition-patterns-by-scope'
 
+import { partitionPatternsByScope } from './partition-patterns-by-scope'
 import { computeNodeParentName } from './compute-node-parent-name'
 import { matches } from '../../utils/matches'
 
@@ -30,15 +32,74 @@ export function passesDeclarationMatchesPatternFilter({
   if (!declarationMatchesPattern) {
     return true
   }
-  let [parentNode] = parentNodes
 
-  /* v8 ignore if -- @preserve Unsure how we can reach that case */
-  if (!parentNode) {
+  let { shallowScopePatterns, deepScopePatterns } = partitionPatternsByScope(
+    declarationMatchesPattern,
+  )
+
+  return (
+    matchesShallowScopedExpressions({
+      patterns: shallowScopePatterns,
+      parentNodes,
+      sourceCode,
+    }) ||
+    matchesDeepScopedExpressions({
+      patterns: deepScopePatterns,
+      parentNodes,
+      sourceCode,
+    })
+  )
+}
+
+function matchesShallowScopedExpressions({
+  parentNodes,
+  sourceCode,
+  patterns,
+}: {
+  parentNodes: ObjectTypeParentForDeclarationMatch[]
+  sourceCode: TSESLint.SourceCode
+  patterns: SingleRegexOption[]
+}): boolean {
+  let [firstParent] = parentNodes
+  // v8 ignore if -- @preserve Unsure how we can reach that case
+  if (!firstParent) {
     return false
   }
 
-  return matches(
-    computeNodeParentName(parentNode, sourceCode),
-    declarationMatchesPattern,
+  return matchesParentExpression({
+    propertyExpression: firstParent,
+    sourceCode,
+    patterns,
+  })
+}
+
+function matchesDeepScopedExpressions({
+  parentNodes,
+  sourceCode,
+  patterns,
+}: {
+  parentNodes: ObjectTypeParentForDeclarationMatch[]
+  sourceCode: TSESLint.SourceCode
+  patterns: SingleRegexOption[]
+}): boolean {
+  return parentNodes.some(parentNode =>
+    matchesParentExpression({
+      propertyExpression: parentNode,
+      sourceCode,
+      patterns,
+    }),
   )
+}
+
+function matchesParentExpression({
+  propertyExpression,
+  sourceCode,
+  patterns,
+}: {
+  propertyExpression: ObjectTypeParentForDeclarationMatch
+  sourceCode: TSESLint.SourceCode
+  patterns: SingleRegexOption[]
+}): boolean {
+  let parentName = computeNodeParentName(propertyExpression, sourceCode)
+  return patterns.some(pattern => matches(parentName, pattern))
 }
