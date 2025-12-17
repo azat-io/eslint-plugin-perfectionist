5cade284a4e760736f6fdface8cde2b355270492
Azat S. <to@azat.io>
2025-12-17 20:34:31 +0300

    fix: add js extensions to relative imports in decl files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,36 @@ import dts from 'vite-plugin-dts'
 import path from 'node:path'
 
 export default defineConfig({
+  plugins: [
+    dts({
+      beforeWriteFile: (filePath, content) => ({
+        content: content.replaceAll(
+          /(?<importPath>(?:from|import\s*\()\s*["']\..*?)(?<quote>["'])/gu,
+          (...replaceArguments) => {
+            let { importPath, quote } = replaceArguments.at(-1) as {
+              importPath: string
+              quote: string
+            }
+            if (importPath.endsWith('.js') || importPath.endsWith('.json')) {
+              return `${importPath}${quote}`
+            }
+            return `${importPath}.js${quote}`
+          },
+        ),
+        filePath,
+      }),
+      include: [
+        path.join(import.meta.dirname, 'index.ts'),
+        path.join(import.meta.dirname, 'types'),
+        path.join(import.meta.dirname, 'rules'),
+        path.join(import.meta.dirname, 'utils'),
+      ],
+      insertTypesEntry: true,
+      copyDtsFiles: true,
+      strictOutput: true,
+    }),
+    prettierFormat(),
+  ],
   build: {
     lib: {
       entry: [
@@ -23,18 +53,4 @@ export default defineConfig({
     },
     minify: false,
   },
-  plugins: [
-    dts({
-      include: [
-        path.join(import.meta.dirname, 'index.ts'),
-        path.join(import.meta.dirname, 'types'),
-        path.join(import.meta.dirname, 'rules'),
-        path.join(import.meta.dirname, 'utils'),
-      ],
-      insertTypesEntry: true,
-      copyDtsFiles: true,
-      strictOutput: true,
-    }),
-    prettierFormat(),
-  ],
 })
