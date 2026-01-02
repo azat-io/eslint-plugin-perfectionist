02d3914786735f88feca0c18a43b510b5fcad58f
hugo.prunaux <hugo.prunaux@gmail.com>
2026-01-02 18:42:08 +0100

    refactor: move `buildUsageComparator` in new file

--- rules/sort-modules/build-comparator-by-options-computer.ts
+++ rules/sort-modules/build-comparator-by-options-computer.ts
@@ -1,18 +1,9 @@
-import type {
-  ComparatorByOptionsComputer,
-  Comparator,
-} from '../../utils/compare/default-comparator-by-options-computer'
-import type {
-  SortModulesSortingNode,
-  SortModulesOptions,
-  SortModulesNode,
-} from './types'
+import type { ComparatorByOptionsComputer } from '../../utils/compare/default-comparator-by-options-computer'
+import type { SortModulesSortingNode, SortModulesOptions } from './types'
 
 import { defaultComparatorByOptionsComputer } from '../../utils/compare/default-comparator-by-options-computer'
-import { sortNodesByDependencies } from '../../utils/sort-nodes-by-dependencies'
-import { computeOrderedValue } from '../../utils/compare/compute-ordered-value'
 import { UnreachableCaseError } from '../../utils/unreachable-case-error'
-import { computeDependencies } from './compute-dependencies'
+import { buildUsageComparator } from './build-usage-comparator'
 
 export function buildComparatorByOptionsComputer({
   ignoreEslintDisabledNodes,
@@ -47,48 +38,5 @@ export function buildComparatorByOptionsComputer({
       default:
         throw new UnreachableCaseError(options.type)
     }
-  }
-}
-
-function buildUsageComparator({
-  ignoreEslintDisabledNodes,
-  sortingNodes,
-  options,
-}: {
-  options: Required<SortModulesOptions[number]>
-  sortingNodes: SortModulesSortingNode[]
-  ignoreEslintDisabledNodes: boolean
-}): Comparator<SortModulesSortingNode> {
-  let orderByNode = buildOrderByNodeMap()
-  return (a, b) => {
-    let nodeA = a.node
-    let nodeB = b.node
-
-    let orderA = orderByNode.get(nodeA)!
-    let orderB = orderByNode.get(nodeB)!
-
-    return computeOrderedValue(orderA - orderB, options.order)
-  }
-
-  function buildOrderByNodeMap(): Map<SortModulesNode, number> {
-    let sortingNodesWithUpdatedDependencies = sortingNodes.map(
-      ({ isEslintDisabled, dependencyNames, node }) => ({
-        dependencies: computeDependencies(node, { type: 'soft' }),
-        isEslintDisabled,
-        dependencyNames,
-        node,
-      }),
-    )
-    let sortedSortingNodes = sortNodesByDependencies(
-      sortingNodesWithUpdatedDependencies,
-      { ignoreEslintDisabledNodes },
-    )
-
-    let orderByNodeMap = new Map<SortModulesNode, number>()
-    for (let [i, { node }] of sortedSortingNodes.entries()) {
-      orderByNodeMap.set(node, i)
-    }
-
-    return orderByNodeMap
   }
 }
---
+++ rules/sort-modules/build-usage-comparator.ts
@@ -0,0 +1,61 @@
+import type { TSESTree } from '@typescript-eslint/types'
+
+import type { Comparator } from '../../utils/compare/default-comparator-by-options-computer'
+import type { SortModulesSortingNode, SortModulesOptions } from './types'
+
+import { sortNodesByDependencies } from '../../utils/sort-nodes-by-dependencies'
+import { computeOrderedValue } from '../../utils/compare/compute-ordered-value'
+import { computeDependencies } from './compute-dependencies'
+
+/**
+ * Builds a comparator function for sorting module nodes based on their usage.
+ *
+ * @param params - The parameters object.
+ * @param params.ignoreEslintDisabledNodes - Whether to ignore ESLint disabled
+ *   nodes.
+ * @param params.sortingNodes - The module sorting nodes.
+ * @param params.options - The sorting options.
+ * @returns A comparator function for sorting module nodes by usage.
+ */
+export function buildUsageComparator({
+  ignoreEslintDisabledNodes,
+  sortingNodes,
+  options,
+}: {
+  options: Required<SortModulesOptions[number]>
+  sortingNodes: SortModulesSortingNode[]
+  ignoreEslintDisabledNodes: boolean
+}): Comparator<SortModulesSortingNode> {
+  let orderByNode = buildOrderByNodeMap()
+  return (a, b) => {
+    let nodeA = a.node
+    let nodeB = b.node
+
+    let orderA = orderByNode.get(nodeA)!
+    let orderB = orderByNode.get(nodeB)!
+
+    return computeOrderedValue(orderA - orderB, options.order)
+  }
+
+  function buildOrderByNodeMap(): Map<TSESTree.ProgramStatement, number> {
+    let sortingNodesWithUpdatedDependencies = sortingNodes.map(
+      ({ isEslintDisabled, dependencyNames, node }) => ({
+        dependencies: computeDependencies(node, { type: 'soft' }),
+        isEslintDisabled,
+        dependencyNames,
+        node,
+      }),
+    )
+    let sortedSortingNodes = sortNodesByDependencies(
+      sortingNodesWithUpdatedDependencies,
+      { ignoreEslintDisabledNodes },
+    )
+
+    let orderByNodeMap = new Map<TSESTree.ProgramStatement, number>()
+    for (let [i, { node }] of sortedSortingNodes.entries()) {
+      orderByNodeMap.set(node, i)
+    }
+
+    return orderByNodeMap
+  }
+}
