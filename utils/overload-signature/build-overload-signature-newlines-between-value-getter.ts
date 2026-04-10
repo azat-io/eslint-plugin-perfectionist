8bc4256c22f4d50c9e0ef929b709a71223e7b58a
hugo.prunaux <hugo.prunaux@gmail.com>
2026-04-10 17:43:42 +0200

    feat: update `buildOverloadSignatureNewlinesBetweenValueGetter`
    - And add tests.

--- utils/overload-signature/build-overload-signature-newlines-between-value-getter.ts
+++ utils/overload-signature/build-overload-signature-newlines-between-value-getter.ts
@@ -2,15 +2,20 @@ import type { TSESTree } from '@typescript-eslint/types'
 
 import type { SortingNodeWithOverloadSignatureImplementation } from './overload-signature-group'
 import type { NewlinesBetweenValueGetter } from '../get-newlines-between-errors'
+import type { NewlinesBetweenOption } from '../../types/common-groups-options'
 
 /**
  * Newlines between value getter for overload signatures.
  *
+ * @param newlinesBetweenOverloadSignatures - Newlines between overload
+ *   signatures option value.
  * @returns NewlinesBetween option value.
  */
 export function buildOverloadSignatureNewlinesBetweenValueGetter<
   T extends TSESTree.Node,
->(): NewlinesBetweenValueGetter<
+>(
+  newlinesBetweenOverloadSignatures: NewlinesBetweenOption,
+): NewlinesBetweenValueGetter<
   SortingNodeWithOverloadSignatureImplementation<T>
 > {
   return ({ computedNewlinesBetween, right, left }) => {
@@ -19,7 +24,7 @@ export function buildOverloadSignatureNewlinesBetweenValueGetter<
       left.overloadSignatureImplementation ===
         right.overloadSignatureImplementation
     ) {
-      return 0
+      return newlinesBetweenOverloadSignatures
     }
     return computedNewlinesBetween
   }
