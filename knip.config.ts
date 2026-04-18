422e8f2f5ec7921eaae689cc79cac365f6a64cfb
Azat S. <to@azat.io>
2026-04-18 09:14:34 +0300

    chore: update dependencies

--- knip.config.ts
+++ knip.config.ts
@@ -1,7 +1,6 @@
 import type { KnipConfig } from 'knip'
 
 export default {
-  entry: ['index.ts', 'test/v8-coverage-provider.js'],
-  ignoreDependencies: ['@vitest/coverage-custom'],
   ignore: ['test/fixtures/**'],
+  entry: ['index.ts'],
 } satisfies KnipConfig
