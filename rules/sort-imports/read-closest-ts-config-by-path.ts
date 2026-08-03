93a267e46c978e27bd5bf4c22ec7e26acda57bf2
Hugo <60015232+hugop95@users.noreply.github.com>
2026-08-03 15:15:29 +0200

    fix: take into account extended tsconfigs

--- rules/sort-imports/read-closest-ts-config-by-path.ts
+++ rules/sort-imports/read-closest-ts-config-by-path.ts
@@ -136,34 +136,24 @@ function getCompilerOptions(
   }
 
   let parsedContent = typescriptImport.parseJsonConfigFileContent(
-    configFileRead,
+    configFileRead.config,
     typescriptImport.sys,
     path.dirname(filePath),
   )
-
-  let compilerOptionsConverted =
-    typescriptImport.convertCompilerOptionsFromJson(
-      // eslint-disable-next-line typescript/no-unsafe-member-access
-      parsedContent.raw.config.compilerOptions,
-      path.dirname(filePath),
-    )
-
-  if (compilerOptionsConverted.errors.length > 0) {
+  if (parsedContent.errors.length > 0) {
     throw new Error(
-      `Error getting compiler options: ${JSON.stringify(
-        compilerOptionsConverted.errors,
-      )}`,
+      `Error getting compiler options: ${JSON.stringify(parsedContent.errors)}`,
     )
   }
 
   let cache = typescriptImport.createModuleResolutionCache(
     contextCwd,
     fileName => typescriptImport.sys.resolvePath(fileName),
-    compilerOptionsConverted.options,
+    parsedContent.options,
   )
 
   let output: ReadClosestTsConfigByPathValue = {
-    compilerOptions: compilerOptionsConverted.options,
+    compilerOptions: parsedContent.options,
     cache,
   }
 
