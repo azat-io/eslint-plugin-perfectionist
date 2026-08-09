e3cc1bb83e57a857d61eaffc15997c718e486bd1
hugo.prunaux <hugo.prunaux@gmail.com>
2026-08-09 17:34:59 +0200

    refactor: create `isNodeInsideDeferredFunction`

--- rules/sort-classes/compute-dependencies-by-sorting-node.ts
+++ rules/sort-classes/compute-dependencies-by-sorting-node.ts
@@ -8,10 +8,9 @@ import type { SortClassesSortingNode, NodeNameDetails } from './types'
 import type { RegexOption } from '../../types/common-options'
 
 import { computeDependenciesBySortingNode as baseComputeDependenciesBySortingNode } from '../../utils/compute-dependencies-by-sorting-node'
-import { computeParentNodesWithTypes } from '../../utils/compute-parent-nodes-with-types'
+import { isNodeInsideDeferredFunction } from '../../utils/is-node-inside-deferred-function'
 import { computeIdentifierNameDetails } from './compute-identifier-name-details'
 import { UnreachableCaseError } from '../../utils/unreachable-case-error'
-import { matches } from '../../utils/matches'
 
 type SortingNodeWithoutDependencies = Omit<
   SortClassesSortingNode,
@@ -71,7 +70,14 @@ function computeIdentifierOrThisExpressionDependency({
   sortingNodes: SortingNodeWithoutDependencies[]
   classElement: TSESTree.ClassElement
 }): SortingNodeWithoutDependencies | null {
-  if (shouldIgnoreCallbackDependency()) {
+  if (
+    classElement.type !== AST_NODE_TYPES.StaticBlock &&
+    isNodeInsideDeferredFunction({
+      ignoreCallbackDependenciesPatterns,
+      maxParent: classElement,
+      node,
+    })
+  ) {
     return null
   }
 
@@ -104,28 +110,6 @@ function computeIdentifierOrThisExpressionDependency({
       /* v8 ignore next 2 -- @preserve Unhandled cases */
       default:
         return null
-    }
-  }
-  function shouldIgnoreCallbackDependency(): boolean {
-    let callExpressionParents = computeParentNodesWithTypes({
-      allowedTypes: [AST_NODE_TYPES.CallExpression],
-      maxParent: classElement,
-      consecutiveOnly: false,
-      node,
-    })
-
-    return callExpressionParents.some(shouldIgnoreCallExpression)
-
-    function shouldIgnoreCallExpression(
-      callExpressionParent: TSESTree.CallExpression,
-    ): boolean {
-      return (
-        'name' in callExpressionParent.callee &&
-        matches(
-          callExpressionParent.callee.name,
-          ignoreCallbackDependenciesPatterns,
-        )
-      )
     }
   }
 }
@@ -210,16 +194,12 @@ function shouldIgnoreDependencyComputation(
 ): boolean {
   switch (node.type) {
     case AST_NODE_TYPES.TSAbstractPropertyDefinition:
-    case AST_NODE_TYPES.TSAbstractMethodDefinition:
-    case AST_NODE_TYPES.StaticBlock:
-      return false
     case AST_NODE_TYPES.TSAbstractAccessorProperty:
+    case AST_NODE_TYPES.TSAbstractMethodDefinition:
     case AST_NODE_TYPES.PropertyDefinition:
     case AST_NODE_TYPES.AccessorProperty:
-      return (
-        node.value?.type === AST_NODE_TYPES.ArrowFunctionExpression ||
-        node.value?.type === AST_NODE_TYPES.FunctionExpression
-      )
+    case AST_NODE_TYPES.StaticBlock:
+      return false
     case AST_NODE_TYPES.MethodDefinition:
     case AST_NODE_TYPES.TSIndexSignature:
       return true
