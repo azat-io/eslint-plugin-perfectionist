7e5541e034e5e4746ae1e2cd0ab1662125613a3e
Azat S. <to@azat.io>
2026-09-05 22:27:53 +0800

    chore: add file extension to vite config import

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, mergeConfig } from 'vitest/config'
 
-import viteConfig from './vite.config'
+import viteConfig from './vite.config.ts'
 
 export default defineConfig(
   mergeConfig(viteConfig, {
